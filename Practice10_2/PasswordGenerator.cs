e3bf69681b8678cd90f9a5cdf432b2d73695b545
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-07-23 19:07:42 +0500

    1 and 2 task is done

---
+++ Practice10_2/PasswordGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice10_2
+{
+    public class PasswordGenerator
+    {
+        Random rd = new Random();
+        char[] numbers = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+        char[] symbols = new char[] { '!', '"', '#', '$', '%', '&', '\'','(',')','*','+',',','/','-','^' };
+        List<string> password = new List<string>();
+        public string Generate(bool hasDigits, bool hasLetters, bool hasSymbol, int length)
+        {
+            for (int i = 0; i <= length;)
+            {
+                if (hasDigits)
+                {
+                    password.Add(Convert.ToString(numbers[rd.Next(0, numbers.Length)]));
+                    i++;
+                }
+                if (hasLetters)
+                {
+                    password.Add(Convert.ToString(Convert.ToChar(rd.Next(96, 122))));
+                    i++;
+                }
+                if (hasSymbol)
+                {
+                    i++;
+                    password.Add(Convert.ToString(symbols[rd.Next(0, numbers.Length)]));
+                }
+                else { i++; }
+            }
+            try 
+            {
+                string pass = "";
+                for (int i = length; i > 0; i--)
+                {
+                    for (int j = 0; j < password.Count - 1; j++)
+                    {
+                        var temp = password[j];
+                        password[j] = password[i];
+                        password[i] = temp;
+                    }
+                    pass += password[i];
+                }
+
+                return pass;
+            }
+            catch (Exception )
+            {
+                return "password not created";
+            }
+        }
+    }
+
+}
---
+++ Practice10_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using Practice10_2;
+
+namespace Mysolutions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("parolda raqamlar bulsinmi[y/n]");
+            bool hasdigit = true ? Console.ReadLine().ToLower() == "y": false;
+            
+            Console.WriteLine("parolda harflar bulsinmi[y/n]");
+            bool hasletter = true ? Console.ReadLine().ToLower() == "y" : false;
+
+            Console.WriteLine("parolda simbollar bulsinmi[y/n]");
+            bool hassymbol = true ? Console.ReadLine().ToLower() == "y" : false;
+
+            Console.WriteLine("Enter a password length");
+            var length = Convert.ToInt32(Console.ReadLine());
+
+
+            var Passgenerat = new PasswordGenerator();
+            var password = Passgenerat.Generate(hasdigit,hasletter,hassymbol,length);
+
+            Console.WriteLine(password);
+        }
+    }
+}
