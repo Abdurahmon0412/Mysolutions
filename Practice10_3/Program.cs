51694c7032d0ad3271854809b12a2c6242565da9
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-07-30 12:03:05 +0500

    11 is full dane

--- Practice10_3/Program.cs
+++ Practice10_3/Program.cs
@@ -1,11 +1,4 @@
 ﻿using Practice10_3;
-using System.ComponentModel.DataAnnotations;
-namespace Mysolutions
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
             
 
         }
